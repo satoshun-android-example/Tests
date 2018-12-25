fce04f033a1c65acb2b9b7603350089b3daf84f5
SatoShun <shun.sato1@gmail.com>
2018-12-25 12:41:51 +0000

    update libs

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,59 +1,59 @@
 object Vers {
-  val compile_sdk = 28
-  val min_sdk = 26
-  val target_sdk = 28
+  const val compile_sdk = 28
+  const val min_sdk = 26
+  const val target_sdk = 28
 
-  val kotlin = "1.3.0"
-  val couroutine = "1.0.0"
+  const val kotlin = "1.3.11"
+  const val couroutine = "1.0.1"
 }
 
 object Libs {
-  val android_plugin = "com.android.tools.build:gradle:3.2.1"
-  val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Vers.kotlin}"
-  val versions_plugin = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
+  const val android_plugin = "com.android.tools.build:gradle:3.4.0-alpha09"
+  const val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Vers.kotlin}"
+  const val versions_plugin = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
 
-  val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Vers.kotlin}"
-  val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Vers.couroutine}"
-  val ui_coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Vers.couroutine}"
+  const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Vers.kotlin}"
+  const val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Vers.couroutine}"
+  const val ui_coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Vers.couroutine}"
 
-  val ktx = "androidx.core:core-ktx:1.0.0"
+  const val ktx = "androidx.core:core-ktx:1.0.0"
 
-  val appcompat = "androidx.appcompat:appcompat:1.0.0"
-  val recyclerview = "androidx.recyclerview:recyclerview:1.0.0"
-  val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha2"
-  val cardview = "androidx.cardview:cardview:1.0.0"
+  const val appcompat = "androidx.appcompat:appcompat:1.0.0"
+  const val recyclerview = "androidx.recyclerview:recyclerview:1.0.0"
+  const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha3"
+  const val cardview = "androidx.cardview:cardview:1.0.0"
 
-  val material = "com.google.android.material:material:1.0.0"
-  val coordinatorlayout = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
-  val paging = "androidx.paging:paging-runtime:2.0.0-rc01"
-  val navigation = "android.arch.navigation:navigation-fragment-ktx:1.0.0"
-  val navigation_ui = "android.arch.navigation:navigation-ui-ktx:1.0.0"
+  const val material = "com.google.android.material:material:1.0.0"
+  const val coordinatorlayout = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
+  const val paging = "androidx.paging:paging-runtime:2.0.0-rc01"
+  const val navigation = "android.arch.navigation:navigation-fragment-ktx:1.0.0"
+  const val navigation_ui = "android.arch.navigation:navigation-ui-ktx:1.0.0"
 
-  val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
-  val livedata = "androidx.lifecycle:lifecycle-livedata:2.0.0"
-  val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:2.0.0"
-  val lifecycle_extensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
+  const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+  const val livedata = "androidx.lifecycle:lifecycle-livedata:2.0.0"
+  const val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:2.0.0"
+  const val lifecycle_extensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
 
-  val emoji = "androidx.emoji:emoji:1.0.0"
-  val emoji_compat = "androidx.emoji:emoji-appcompat:1.0.0"
-  val emoji_bundled = "androidx.emoji:emoji-bundled:1.0.0"
+  const val emoji = "androidx.emoji:emoji:1.0.0"
+  const val emoji_compat = "androidx.emoji:emoji-appcompat:1.0.0"
+  const val emoji_bundled = "androidx.emoji:emoji-bundled:1.0.0"
 
-  val coroutine_binding = "com.github.satoshun.coroutinebinding:coroutinebinding:0.1.1"
+  const val coroutine_binding = "com.github.satoshun.coroutinebinding:coroutinebinding:0.1.1"
 
-  val multidex = "com.android.support:multidex:1.0.3"
+  const val multidex = "com.android.support:multidex:1.0.3"
 
-  val rxjava = "io.reactivex.rxjava2:rxjava:2.2.0"
-  val rxandroid = "io.reactivex.rxjava2:rxandroid:2.1.0"
-  val rxkotlin = "io.reactivex.rxjava2:rxkotlin:2.3.0"
-  val rxbinding = "com.jakewharton.rxbinding2:rxbinding:2.2.0"
+  const val rxjava = "io.reactivex.rxjava2:rxjava:2.2.0"
+  const val rxandroid = "io.reactivex.rxjava2:rxandroid:2.1.0"
+  const val rxkotlin = "io.reactivex.rxjava2:rxkotlin:2.3.0"
+  const val rxbinding = "com.jakewharton.rxbinding2:rxbinding:2.2.0"
 
-  val junit = "junit:junit:4.12"
-  val truth = "com.google.truth:truth:0.42"
-  val test_rules = "androidx.test:rules:1.1.0"
-  val test_runner = "androidx.test:runner:1.1.0"
-  val test_junit_rules = "androidx.test.ext:junit:1.0.0"
-  val test_core = "androidx.test:core:1.0.0"
-  val mockito_kotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.0.0"
-  val espresso = "androidx.test.espresso:espresso-core:3.1.0"
-  val robolectric = "org.robolectric:robolectric:4.0.1"
+  const val junit = "junit:junit:4.12"
+  const val truth = "com.google.truth:truth:0.42"
+  const val test_rules = "androidx.test:rules:1.1.1"
+  const val test_runner = "androidx.test:runner:1.1.1"
+  const val test_junit_rules = "androidx.test.ext:junit:1.1.0"
+  const val test_core = "androidx.test:core:1.1.0"
+  const val mockito_kotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.0.0"
+  const val espresso = "androidx.test.espresso:espresso-core:3.1.0"
+  const val robolectric = "org.robolectric:robolectric:4.1"
 }

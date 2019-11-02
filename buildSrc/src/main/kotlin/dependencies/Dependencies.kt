ad914a7c2cee413739fd96ea47b14c79ca421925
SatoShun <shun.sato1@gmail.com>
2019-11-02 22:59:24 +0000

    update

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -1,13 +1,13 @@
 package dependencies
 
-const val COMPILE_SDK = 28
+const val COMPILE_SDK = 29
 const val MIN_SDK = 26
-const val TARGET_SDK = 28
+const val TARGET_SDK = 29
 
-private const val VKOTLIN = "1.3.21"
+private const val VKOTLIN = "1.3.50"
 private const val VCOUROUTINE = "1.1.0"
 
-const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.5.0-alpha05"
+const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.5.1"
 const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VKOTLIN"
 const val VERSIONS_PLUGIN = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
 
@@ -16,12 +16,12 @@ const val COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VCOUROUTIN
 const val UI_COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VCOUROUTINE"
 
 const val KTX = "androidx.core:core-ktx:1.0.0"
-const val ACTIVITYX = "androidx.activity:activity-ktx:1.0.0-alpha03"
-private const val VFRAGMENTX = "1.1.0-alpha03"
+const val ACTIVITYX = "androidx.activity:activity-ktx:1.0.0"
+private const val VFRAGMENTX = "1.1.0"
 const val FRAGMENT = "androidx.fragment:fragment:$VFRAGMENTX"
 const val FRAGMENTX = "androidx.fragment:fragment-ktx:$VFRAGMENTX"
 
-const val APPCOMPAT = "androidx.appcompat:appcompat:1.0.0"
+const val APPCOMPAT = "androidx.appcompat:appcompat:1.1.0"
 const val RECYCLERVIEW = "androidx.recyclerview:recyclerview:1.0.0"
 const val CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-alpha3"
 const val CARDVIEW = "androidx.cardview:cardview:1.0.0"
@@ -29,7 +29,7 @@ const val CARDVIEW = "androidx.cardview:cardview:1.0.0"
 const val MATERIAL = "com.google.android.material:material:1.0.0"
 const val COORDINATORLAYOUT = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
 
-const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.1.0"
 const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata:2.0.0"
 const val LIFECYCLE_COMPILER = "androidx.lifecycle:lifecycle-compiler:2.0.0"
 const val LIFECYCLE_EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:2.0.0"
